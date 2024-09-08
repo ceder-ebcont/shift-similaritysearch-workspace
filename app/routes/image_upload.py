817c5cfe3798e77a6aeb310e239d8734670f879b
christoph.eder <office@christopheder.eu>
2024-09-08 15:28:17 +0200

    Added boilerplate workshop code

--- app/routes/image_upload.py
+++ app/routes/image_upload.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request
 from app.embedding import get_image_embedding
 from app.elasticsearch_helper import save_image
-from app.routes.image_similarity_search import convert_image_to_data_uri
+from app.helper_functions import convert_image_to_data_uri
 from PIL import Image
 
 image_upload_routes = Blueprint('image_upload_routes', __name__)
@@ -16,8 +16,13 @@ def upload():
                 image = Image.open(uploaded_file)
                 image_id = uploaded_file.filename
 
+                #Converts an image into a data URI in Base64 format
                 image_data = convert_image_to_data_uri(image)
+                
+                #Get embedding for the image
                 embedding = get_image_embedding(image)
+                
+                #Save image in the cluster
                 save_image(image_id, embedding, image_data)
 
         message = "Upload successful!"
